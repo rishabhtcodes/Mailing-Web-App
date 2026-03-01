15b67b938b13f2bb516aa34a473186442afe9b99
Rishabh <rishabhtiwari3538@gmail.com>
2026-03-01 19:47:26 +0530

    feat: Initialize Django project with a custom user app, authentication flows, and foundational templates.

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    # Override the default id field to be compatible with MongoDB
     id = models.CharField(max_length=24, primary_key=True, editable=False)
     email = models.EmailField(unique=True)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']  # username still needed for Django admin
+    REQUIRED_FIELDS = ['username']
     
     class Meta:
         db_table = 'auth_user'
@@ -16,7 +15,6 @@ class CustomUser(AbstractUser):
         if not self.id:
             from bson.objectid import ObjectId
             self.id = str(ObjectId())
-        # Ensure username is set from email if not provided
         if not self.username:
             self.username = self.email.split('@')[0]
         super().save(*args, **kwargs)
